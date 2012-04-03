1f15eb608f856dde6369f96bd7426994b9bf9856
Federico Spini <federico.spini@gmail.com>
2012-04-03 16:40:15 +0200

    updated cantilever color

--- lessons/2012-04-02/2012-04-02.js
+++ lessons/2012-04-02/2012-04-02.js
@@ -1,44 +1,44 @@
 var pillars = SIMPLEX_GRID([
-	REPLICA(3)([0.15,-6*2.4,0.15]),
-	[0.15,-6*2.4,0.15],
-	[1.5,3,3]
+  REPLICA(3)([0.15,-6*2.4,0.15]),
+  [0.15,-6*2.4,0.15],
+  [1.5,3,3]
 ]);
 
 var beams = SIMPLEX_GRID([
-	REPLICA(3)([0.15,-6*2.4,0.15]),
-	[14.7],
-	[-7.5,1.5]
+  REPLICA(3)([0.15,-6*2.4,0.15]),
+  [14.7],
+  [-7.5,1.5]
 ]);
 
 var steelFrames = COLOR([0.2,0.2,0.2])(STRUCT([pillars,beams]));
 
 var floors = SIMPLEX_GRID([
-	REPLICA(3)(14.7),
-	[-0.15,14.4,-0.15],
-	[-1.2,0.3,-2.7,0.3,-2.7,0.3]
+  REPLICA(3)(14.7),
+  [-0.15,14.4,-0.15],
+  [-1.2,0.3,-2.7,0.3,-2.7,0.3]
 ]);
 
 var cantileverFloor = SIMPLEX_GRID([
-	[0.15,2*2.4,0.15],
-	[-0.15,14.4,-0.15],
-	[-1.2,0.3,-2.7,0.3,-2.7,0.3]
+  [0.15,2*2.4,0.15],
+  [-0.15,14.4,-0.15],
+  [-1.2,0.3,-2.7,0.3,-2.7,0.3]
 ]);
 
 var cantileverPillars = SIMPLEX_GRID([
-	[0.15],
-	[0.15,-14.4,0.15],
-	[1.5,3,3]
+  [0.15],
+  [0.15,-14.4,0.15],
+  [1.5,3,3]
 ]);
 
 var cantileverBeams = SIMPLEX_GRID([
-	[0.15],
-	[14.7],
-	[-7.5,1.5]
+  [0.15],
+  [14.7],
+  [-7.5,1.5]
 ]);
 
-var cantilever = STRUCT(
-	[cantileverFloor,cantileverPillars,cantileverBeams]
-);
+var cantileverSteelFrames = COLOR([0.2,0.2,0.2])(STRUCT([cantileverPillars,cantileverBeams]));
+
+var cantilever = STRUCT([cantileverFloor,cantileverSteelFrames]);
 
 var cantilever1 = S([0])([-1])(cantilever);
 var cantilever2 = T([0])([3*14.7])(cantilever);
@@ -54,12 +54,12 @@ var frameGroup = function(number,color) {return STRUCT( REPLICA(number)([ frame(
 
 var colors = [[1,0,0],[0,1,0],[0,0,1],[0,1,1],[1,0,1],[1,1,0]];
 var frames = STRUCT([
-	frameGroup(3,colors[0]), T([0])([3*2.4]),
-	frameGroup(3,colors[1]), T([0])([3*2.4]), T([0])([0.3]),
-	frameGroup(3,colors[2]), T([0])([3*2.4]),
-	frameGroup(3,colors[3]), T([0])([3*2.4]), T([0])([0.3]),
-	frameGroup(3,colors[4]), T([0])([3*2.4]),
-	frameGroup(3,colors[5])
+  frameGroup(3,colors[0]), T([0])([3*2.4]),
+  frameGroup(3,colors[1]), T([0])([3*2.4]), T([0])([0.3]),
+  frameGroup(3,colors[2]), T([0])([3*2.4]),
+  frameGroup(3,colors[3]), T([0])([3*2.4]), T([0])([0.3]),
+  frameGroup(3,colors[4]), T([0])([3*2.4]),
+  frameGroup(3,colors[5])
 ]);
 
 var backFrames = T([1])([14.7])(S([1])([-1])(frames));
